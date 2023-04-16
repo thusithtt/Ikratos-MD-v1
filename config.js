ff39810033830b58f144f487b0498205ffe0fec4
thusithtt <126143448+thusithtt@users.noreply.github.com>
2023-04-16 22:14:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -53,10 +53,10 @@ global.ppulsa = '085736178354' //Nomor SimCard Yang Kamu Pake
 global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada Ketik - aja
 
 /*============== NOMOR ==============*/
-global.nomorbot = '68673045381' //Nomor Bot
-global.nomorown = '923470027813' //Nomor Owner
-global.namebot = 'Ikratos-MD-v1' //Nama Bot
-global.nameown = 'ᴀʜᴍᴀᴅ ᴀʟɪ🇵🇰' // Nama Owner
+global.nomorbot = '94711266526' //Nomor Bot
+global.nomorown = '94711266526' //Nomor Owner
+global.namebot = 'thusii bot' //Nama Bot
+global.nameown = 'mr thusi' // Nama Owner
 
 
 /*============== CHATGPT ==============*/
